a1782e5016de8258ff241af8ac33dbd9c832cb59
leaf_ying <leaf_ying@gss.com.tw>
2022-09-03 13:25:26 +0800

    Add dependencies to setup.py

--- setup.py
+++ setup.py
@@ -10,6 +10,6 @@ setup(
     author="leaf_ying,jeffchengtw,patrick0115",
     author_email="leaf.ying.work@gmail.com",
     url="https://github.com/guyleaf/Defect-Detection-ZSI",
-    install_requires=["pytorch-lightning~=1.7.4"],
+    install_requires=["torch~=1.12.1", "torchvision~=0.13.0", "pytorch-lightning~=1.7.4"],
     packages=find_namespace_packages(),
 )
