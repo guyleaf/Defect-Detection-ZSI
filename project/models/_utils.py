4dba619002beb772a7ebadcc032c739671c7ebe5
guyleaf <leaf.ying.work@gmail.com>
2022-09-04 20:49:10 +0800

    Working on RPN implementation

---
+++ project/models/_utils.py
@@ -0,0 +1,33 @@
+﻿from functools import partial
+from typing import Union
+
+import numpy as np
+import torch
+
+
+def load_word_vectors(path: str, delimiter: str = ",", as_torch: bool = False) -> Union[np.ndarray, torch.Tensor]:
+    data = np.loadtxt(path, dtype=np.float32, delimiter=delimiter)
+    if as_torch:
+        data = torch.from_numpy(data)
+    return data
+
+
+def multi_apply(func, *args, **kwargs):
+    """Apply function to a list of arguments.
+    https://github.com/open-mmlab/mmdetection/blob/df28da98926bc410e16bed1e9fc7d425d9a89495/mmdet/core/utils/misc.py#L11
+
+    Note:
+        This function applies the ``func`` to multiple inputs and
+        map the multiple outputs of the ``func`` into different
+        list. Each list contains the same type of outputs corresponding
+        to different inputs.
+    Args:
+        func (Function): A function that will be applied to a list of
+            arguments
+    Returns:
+        tuple(list): A tuple containing multiple list, each list contains \
+            a kind of returned results by the function
+    """
+    pfunc = partial(func, **kwargs) if kwargs else func
+    map_results = map(pfunc, *args)
+    return tuple(map(list, zip(*map_results)))
--- project/models/ba_rpn.py
+++ project/models/ba_rpn.py
@@ -1,11 +1,96 @@
-﻿import torch.nn as nn
+﻿from typing import Optional
+
+import torch
+import torch.nn as nn
+from torchvision.ops import Conv2dNormActivation
+
+from ._utils import load_word_vectors, multi_apply
+from .anchor_utils import AnchorGenerator
 
 
 class BackgroundAwareRPNHead(nn.Module):
-    def __init__(self) -> None:
-        pass
+    _voc: Optional[torch.Tensor]
+
+    def __init__(
+        self,
+        in_channels: int,
+        num_anchors: int,
+        vec_path: str,
+        semantic_channels: int = 300,
+        voc_path: Optional[str] = None,
+    ) -> None:
+        super().__init__()
+
+        # in source code: padding = 1
+        self.conv = Conv2dNormActivation(
+            in_channels, in_channels, 3, norm_layer=None
+        )
+
+        vec, voc = self._load_word_vectors(vec_path, voc_path)
+        bg_vec = vec[:, 0].unsqueeze(-1).unsqueeze(-1)
+
+        # visual feature -> semantic_encoder -> semantice feature -> cls_logits
+        self.semantic_encoder = nn.Conv2d(in_channels, semantic_channels, 1)
+        self.cls_logits = nn.Conv2d(semantic_channels, num_anchors * 2, 1)
+
+        self.bbox_pred = nn.Conv2d(in_channels, num_anchors * 4, 1)
+
+        self.register_buffer("_voc", voc, persistent=False)
+
+        # initialize weights
+        self.apply(self._init_weights)
+        self._init_cls_logits(bg_vec)
+
+    @torch.no_grad()
+    def _init_cls_logits(self, bg_vec: torch.Tensor):
+        weights = self.cls_logits.weight
+        weights.data[::2] = bg_vec.expand(weights.size(0) // 2, -1)
+
+    @torch.no_grad()
+    def _init_weights(self, module: nn.Module) -> None:
+        if isinstance(module, nn.Conv2d):
+            nn.init.normal_(module.weight, std=0.01)
+            if hasattr(module, "bias") and module.bias is not None:
+                nn.init.zeros_(module.bias)
+
+    def _load_word_vectors(
+        self, vec_path: str, voc_path: Optional[str]
+    ) -> tuple[torch.Tensor, Optional[torch.Tensor]]:
+        vec = load_word_vectors(vec_path, as_torch=True)
+        voc = None
+        if voc_path:
+            voc = load_word_vectors(voc_path, as_torch=True)
+
+        return vec, voc
+
+    def forward(
+        self, x: torch.Tensor
+    ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        x = self.conv(x)
+
+        # classification branch
+        semantic_feature = self.semantic_encoder(x)
+        if self._voc:
+            semantic_feature = torch.mm(semantic_feature, self._voc)
+        cls_score = self.cls_logits(semantic_feature)
+
+        # regression branch
+        bbox_pred = self.bbox_pred(x)
+
+        bg_vec = torch.mean(self.cls_logits.weight.data[::2], dim=0)
+        return cls_score, bbox_pred, bg_vec
 
 
 class BackgroundAwareRPN(nn.Module):
-    def __init__(self) -> None:
-        pass
+    head: nn.Module
+    anchor_generator: AnchorGenerator
+
+    def __init__(
+        self, head: nn.Module, anchor_generator: AnchorGenerator
+    ) -> None:
+        self.head = head
+        self.anchor_generator = anchor_generator
+
+    def forward(self, feature_maps: list[torch.Tensor]):
+        cls_scores, bbox_preds, bg_vecs = multi_apply(self.head, feature_maps)
+        self.anchor_generator.grid_anchors()
