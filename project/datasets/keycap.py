f38afb065c6faa6d17e12bee0d08aeeb94c3653f
guyleaf <leaf.ying.work@gmail.com>
2022-09-06 19:01:55 +0800

    Add keycap dataset class and fix COCO format

---
+++ project/datasets/keycap.py
@@ -0,0 +1,45 @@
+﻿import json
+import os
+from typing import Callable, Optional
+
+import torchvision.transforms as transforms
+from pytorch_lightning import LightningDataModule
+from torch.utils.data import DataLoader, Dataset
+
+DEFAULT_TRANSFORMS = transforms.Compose([])
+
+
+class KeycapDataset(Dataset):
+    def __init__(
+        self,
+        image_dir: str,
+        ann_file: Optional[str] = None,
+        transforms: transforms.Compose = DEFAULT_TRANSFORMS,
+        ann_transforms: Callable = None
+    ) -> None:
+        super().__init__()
+        self._image_dir = image_dir
+        self._num_images = len(os.listdir(image_dir))
+        self._transforms = transforms
+        self._ann_transforms = ann_transforms
+        if ann_file:
+            with open(ann_file, "r") as f:
+                self._image_anns = json.load(f)
+
+    def __len__(self):
+        return self._num_images
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self._image_dir, self.img_labels.iloc[idx, 0])
+        image = read_image(img_path)
+        label = self.img_labels.iloc[idx, 1]
+        if self.transform:
+            image = self.transform(image)
+        if self.target_transform:
+            label = self.target_transform(label)
+        return image, label
+
+
+class KeycapDataModule(LightningDataModule):
+    def __init__(self, root_dir: str) -> None:
+        super().__init__()
--- scripts/prepare_data.py
+++ scripts/prepare_data.py
@@ -8,13 +8,9 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-CLASS_MAP = {
-    "black_scratch": 0,
-    "white_scrath": 1,
-    "dent": 2,
-    "background": 255,
-}
-COLOR_MAP = {"0": [255, 0, 0], "1": [0, 255, 0], "2": [0, 0, 255]}
+
+SEEN_CLASSES = ["black_scratch", "white_scrath", "dent"]
+COLOR_MAP = {"1": [255, 0, 0], "2": [0, 255, 0], "3": [0, 0, 255]}
 
 
 class Option:
@@ -23,7 +19,7 @@ class Option:
 
     def initialize(self):
         self.parser.add_argument(
-            "--mode", type=str, default="train", help="train or test"
+            "--mode", type=str, default="train_seen", help="train or test"
         )
         self.parser.add_argument(
             "--data_dir",
@@ -32,7 +28,10 @@ class Option:
             help="dataset folder",
         )
         self.parser.add_argument(
-            "--output_dir", type=str, default="fk", help="output folder"
+            "--output_dir",
+            type=str,
+            default="annotations",
+            help="output folder",
         )
         self.parser.add_argument(
             "--debug", action="store_true", help="debug mode true or false"
@@ -44,9 +43,6 @@ class Option:
         args = vars(self.opt)
 
         self.opt.data_dir = os.path.join(self.opt.data_dir, self.opt.mode)
-        self.opt.output_dir = os.path.join(
-            self.opt.output_dir, self.opt.mode + "_json"
-        )
 
         print("------------ Options -------------")
         for k, v in sorted(args.items()):
@@ -67,22 +63,36 @@ class MyEncoder(json.JSONEncoder):
             return super(MyEncoder, self).default(obj)
 
 
+def get_category_dic(
+    category_list: list[str], supercategory: str = "seen"
+):
+    categories = []
+    for idx, name in enumerate(category_list):
+        category = {"id": idx + 1, "name": name, "supercategory": supercategory}
+        categories.append(category)
+    return categories
+
+
 def get_image_dic(path, id):
     # extract file name
-    (filepath, tempfilename) = os.path.split(path)
+    _, tempfilename = os.path.split(path)
 
     img = cv2.imread(path, cv2.IMREAD_COLOR)
     h, w = img.shape[0], img.shape[1]
     return {"file_name": tempfilename, "height": h, "width": w, "id": id}
 
 
-def get_bbox(mask, id):
-    classes = np.unique(mask)
+def get_bbox(mask, image_id, start_ann_id):
+    gray_scales = np.unique(mask)
     ann_list = []
-    cat_list = []
-    for c in classes:
+    ann_id = start_ann_id
+
+    for gray_scale in gray_scales:
+        if gray_scale == 255:
+            continue
+
         img = copy.deepcopy(mask)
-        img[img != c] = 255
+        img[img != gray_scale] = 255
         img[img != 255] = 0
         img = cv2.bitwise_not(img)
         (
@@ -108,42 +118,42 @@ def get_bbox(mask, id):
                 cv2.RETR_TREE,
                 cv2.CHAIN_APPROX_SIMPLE,
             )
-            # add offsets
+            # add x, y offsets
             for contour in contours:
                 contour[:, :, 0] += x1
                 contour[:, :, 1] += y1
 
+            if len(contours) == 0:
+                print("segmenation is empty. discarded")
+                continue
+
             ann_list.append(
                 {
-                    "segmenation": [contour.flatten().tolist() for contour in contours],
+                    "iscrowd": 0,
+                    "segmenation": [contour.flatten() for contour in contours],
                     "area": area,
                     "bbox": bbox,  # (x, y, w, h)
-                    "category_id": c,
-                    "id": id,
+                    "category_id": gray_scale + 1,
+                    "image_id": image_id,
+                    "id": ann_id,
                 }
             )
-            cat_list.append(
-                {
-                    "supercategory": "seen",
-                    "id": c,
-                    "name": [k for k, v in CLASS_MAP.items() if v == c][0],
-                }
-            )
+            ann_id += 1
 
-    return ann_list, cat_list
+    return ann_list
 
 
-def get_annotation_dic(path, id):
+def get_annotation_dic(path, image_id, start_ann_id=0):
     # extract file name
-    (filepath, tempfilename) = os.path.split(path)
-    (filename, extension) = os.path.splitext(tempfilename)
+    filepath, tempfilename = os.path.split(path)
+    filename, _ = os.path.splitext(tempfilename)
 
     mask = cv2.imread(
         os.path.join(filepath, filename + "_label.png"), cv2.IMREAD_GRAYSCALE
     )
-    ann_list, cate_list = get_bbox(mask, id)
 
-    return ann_list, cate_list
+    ann_list = get_bbox(mask, image_id, start_ann_id)
+    return ann_list
 
 
 def visualization(coco, opt):
@@ -151,36 +161,52 @@ def visualization(coco, opt):
     annotations = coco["annotations"]
 
     for image, annotation in zip(images, annotations):
+        file_name = image["file_name"]
         img = cv2.imread(
             os.path.join(opt.data_dir, image["file_name"]), cv2.IMREAD_COLOR
         )
+        contoured_img = copy.deepcopy(img)
         for object in annotation:
             x, y, w, h = object["bbox"]
-            contours = [np.stack(np.split(np.array(contour), len(contour) // 2)) for contour in object["segmenation"]]
+            contours = [
+                np.stack(np.split(contour, len(contour) // 2))
+                for contour in object["segmenation"]
+            ]
             category_id = object["category_id"]
-            class_name = [k for k, v in CLASS_MAP.items() if v == category_id]
+            class_name = SEEN_CLASSES[category_id - 1]
             color = COLOR_MAP[f"{category_id}"]
+
             cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
-            cv2.drawContours(img, contours, -1, color, 3)
-            cv2.putText(img, class_name[0], (x, y - 10), 0, 1, color)
+            cv2.rectangle(contoured_img, (x, y), (x + w, y + h), color, 2)
+            cv2.drawContours(contoured_img, contours, -1, color, 3)
+            cv2.putText(img, class_name, (x, y - 10), 0, 1, color)
+            cv2.putText(contoured_img, class_name, (x, y - 10), 0, 1, color)
+        plt.figure(figsize=(12, 8))
+        plt.subplot(1, 2, 1)
+        plt.title(file_name)
         plt.imshow(img[:, :, ::-1])
+        plt.subplot(1, 2, 2)
+        plt.title(file_name)
+        plt.imshow(contoured_img[:, :, ::-1])
         plt.show()
 
 
 def main():
     opt = Option().parse()
-    images = list()
-    annotations = list()
-    categories = list()
+    images = []
+    annotations = []
+    categories = get_category_dic(SEEN_CLASSES, "seen")
 
     ############################# make COCO #############################
+    start_ann_id = 0
     for id, file in enumerate(glob.glob(os.path.join(opt.data_dir, "*.bmp"))):
         images.append(get_image_dic(file, id))
-        ann, cat = get_annotation_dic(file, id)
-        annotations.append(ann)
-        categories.append(cat)
+        annotations.append(get_annotation_dic(file, id, start_ann_id))
+        start_ann_id += len(annotations[-1])
 
     coco = {
+        "info": {},
+        "licenses": [],
         "images": images,
         "annotations": annotations,
         "categories": categories,
@@ -191,16 +217,13 @@ def main():
     if not os.path.isdir(opt.output_dir):
         os.makedirs(opt.output_dir)
 
-    string = json.dumps(coco, cls=MyEncoder)
     with open(os.path.join(opt.output_dir, opt.mode + ".json"), "w") as f:
-        f.write(string)
+        json.dump(coco, f, sort_keys=True, cls=MyEncoder)
 
     print("save json to ", os.path.join(opt.output_dir, opt.mode + ".json"))
 
     ############################# create json file #############################
     if opt.debug:
-        with open(os.path.join(opt.output_dir, opt.mode + ".json")) as f:
-            result = json.load(f)
         visualization(coco, opt)
 
 
