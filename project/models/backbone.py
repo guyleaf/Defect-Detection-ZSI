8a82439de4a29e9bdabb33f61b8d368e64bc43fd
guyleaf <leaf.ying.work@gmail.com>
2022-09-06 19:25:24 +0800

    Add backbone options to Extractor class

--- project/models/backbone.py
+++ project/models/backbone.py
@@ -1,14 +1,38 @@
+from typing import Optional
 import torch
 from torch import nn, Tensor
 
-from torchvision.models import ResNet50_Weights
+import torchvision.models as models
 from torchvision.models.detection.backbone_utils import resnet_fpn_backbone
 
 
 class Extractor(nn.Module):
-    def __init__(self):
+    def __init__(
+        self,
+        backbone_name: str = "resnet101",
+        trainable_layers: int = 3,
+        returned_layers: Optional[list[int]] = None,
+    ):
         super(Extractor, self).__init__()
-        self.backbone = resnet_fpn_backbone(backbone_name="resnet50", weights=ResNet50_Weights.DEFAULT)
+        if backbone_name == "resnet34":
+            weights = models.ResNet34_Weights.DEFAULT
+        elif backbone_name == "resnet50":
+            weights = models.ResNet50_Weights.DEFAULT
+        elif backbone_name == "resnet101":
+            weights = models.ResNet101_Weights.DEFAULT
+        elif backbone_name == "resnet152":
+            weights = models.ResNet152_Weights.DEFAULT
+        else:
+            raise ValueError(
+                f"The backbone {backbone_name} is not implemented."
+            )
+
+        self.backbone = resnet_fpn_backbone(
+            backbone_name=backbone_name,
+            weights=weights,
+            trainable_layers=trainable_layers,
+            returned_layers=returned_layers,
+        )
 
     def forward(self, x: Tensor) -> list:
         output = self.backbone(x)
@@ -18,7 +42,8 @@ class Extractor(nn.Module):
 
 if __name__ == "__main__":
     with torch.no_grad():
-        extractor = Extractor()
+        extractor = Extractor(backbone_name="resnet34")
         x = torch.randn(1, 3, 224, 224)
+        # x = torch.randn(1, 3, 500, 500)
         y = extractor(x)
         print(len(y))
