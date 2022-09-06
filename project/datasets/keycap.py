63d02abb2f15794f6d6267035a1538f7e8113576
guyleaf <leaf.ying.work@gmail.com>
2022-09-07 00:14:26 +0800

    Finish Keycap dataloader

--- project/datasets/keycap.py
+++ project/datasets/keycap.py
@@ -1,4 +1,5 @@
-﻿import os
+﻿from math import ceil
+import os
 from typing import Optional
 
 import PIL.Image as Image
@@ -6,14 +7,18 @@ import torch
 import torchvision.transforms as transforms
 from pycocotools.coco import COCO
 from pytorch_lightning import LightningDataModule
-from torch.utils.data import DataLoader, Dataset
+from torch.utils.data import DataLoader, Dataset, random_split
 
 from project.data import ImageAnnotation, ImageMetadata
 
-DEFAULT_TRANSFORMS = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-])
+DEFAULT_TRANSFORMS = transforms.Compose(
+    [
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+        ),
+    ]
+)
 
 
 class KeycapDataset(Dataset):
@@ -115,8 +120,42 @@ class KeycapDataset(Dataset):
 
 
 class KeycapDataModule(LightningDataModule):
-    def __init__(self, root_dir: str) -> None:
+    def __init__(
+        self, root_dir: str, batch_size: int = 64, train_val_ratio: float = 0.9
+    ) -> None:
         super().__init__()
+        self._root_dir = root_dir
+        self._batch_size = batch_size
+        self._train_val_ratio = train_val_ratio
+
+    def setup(self, stage=None):
+        # Assign train/val datasets for use in dataloaders
+        if stage == "fit" or stage is None:
+            img_dir = os.path.join(self._root_dir, "train_seen")
+            ann_file = os.path.join(
+                self._root_dir, "annotations", "train_seen.json"
+            )
+            keycap_full = KeycapDataset(img_dir=img_dir, ann_file=ann_file)
+            num_imgs = len(keycap_full)
+            train_num_imgs = ceil(num_imgs * self._train_val_ratio)
+
+            self.keycap_train, self.keycap_val = random_split(
+                keycap_full, [train_num_imgs, num_imgs - train_num_imgs]
+            )
+
+        # Assign test dataset for use in dataloader(s)
+        if stage == "test" or stage is None:
+            img_dir = os.path.join(self._root_dir, "test_unseen")
+            self.keycap_test = KeycapDataset(img_dir=img_dir)
+
+    def train_dataloader(self):
+        return DataLoader(self.keycap_train, batch_size=self._batch_size)
+
+    def val_dataloader(self):
+        return DataLoader(self.keycap_val, batch_size=self._batch_size)
+
+    def test_dataloader(self):
+        return DataLoader(self.keycap_test, batch_size=self._batch_size)
 
 
 if __name__ == "__main__":
@@ -125,4 +164,9 @@ if __name__ == "__main__":
     )
     ann_file = "E:\\Git\\Defect-Detection-ZSI\\tests\\datasets\\keycap\\annotations\\train_seen.json"
     dataset = KeycapDataset(img_dir=img_dir, ann_file=ann_file)
+    print(dataset[0])
+    img_dir = (
+        "E:\\Git\\Defect-Detection-ZSI\\tests\\datasets\\keycap\\test_unseen"
+    )
+    dataset = KeycapDataset(img_dir=img_dir)
     print(dataset[0])
