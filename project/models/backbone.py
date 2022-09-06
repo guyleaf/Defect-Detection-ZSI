5e73471090ccdbc6152235aa3710468c3edc3137
guyleaf <leaf.ying.work@gmail.com>
2022-09-06 19:10:13 +0800

    Update backbone implementation

--- project/models/backbone.py
+++ project/models/backbone.py
@@ -1,21 +1,24 @@
 import torch
 from torch import nn, Tensor
-from torchvision.models.detection.backbone_utils import resnet_fpn_backbone
 
+from torchvision.models import ResNet50_Weights
+from torchvision.models.detection.backbone_utils import resnet_fpn_backbone
 
 
 class Extractor(nn.Module):
     def __init__(self):
         super(Extractor, self).__init__()
-        self.backbone = resnet_fpn_backbone('resnet50', pretrained = True)
+        self.backbone = resnet_fpn_backbone(backbone_name="resnet50", weights=ResNet50_Weights.DEFAULT)
 
-    def forward(self, x : Tensor) -> list:
+    def forward(self, x: Tensor) -> list:
         output = self.backbone(x)
-        output = [v for k, v in output.items()] 
+        output = list(output.values())
         return output
 
-def test():
-    extractor = Extractor()
-    x = torch.randn(1, 3, 224, 224)
-    y = extractor(x)
-    
+
+if __name__ == "__main__":
+    with torch.no_grad():
+        extractor = Extractor()
+        x = torch.randn(1, 3, 224, 224)
+        y = extractor(x)
+        print(len(y))
