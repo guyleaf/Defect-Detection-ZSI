25611c091f89cec7a70ea77d0dda66bc3db8a60c
jeffchengtw <jeff.cheng.cs10@nycu.edu.tw>
2022-09-06 17:42:33 +0800

    modify

--- project/models/zero_shot_mask_rcnn.py
+++ project/models/zero_shot_mask_rcnn.py
@@ -1,22 +1,10 @@
 ﻿import torch
 import torch.nn as nn
-from torchvision.models.detection.backbone_utils import resnet_fpn_backbone
-import collections
-
-
-
-backbone = resnet_fpn_backbone('resnet50', pretrained = True)
-
 
 class ZeroShotMaskRCNN(nn.Module):
     def __init__(self) -> None:
         pass
 
 
-def test(backbone):
-    extractor = backbone
-    x = torch.randn(1, 3, 224, 224)
-    y = extractor(x)
-    for k, v in y.items():
-        print(k, v.shape)
+
 
